b1de2bec31c12bc371980a571a60b488ddd13057
toocolors <noa.algorithms@gmail.com>
2026-07-15 07:40:21 -0700

    Add post assignment path
    Add post path for creating assignment.

--- src/main/java/com/cst438/controller/AssignmentController.java
+++ src/main/java/com/cst438/controller/AssignmentController.java
@@ -1,8 +1,10 @@
 package com.cst438.controller;
 
 import java.security.Principal;
+import java.sql.Date;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,20 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.Assignment;
 import com.cst438.domain.AssignmentRepository;
 import com.cst438.domain.GradeRepository;
+import com.cst438.domain.Section;
 import com.cst438.domain.SectionRepository;
+import com.cst438.domain.Term;
+import com.cst438.domain.TermRepository;
 import com.cst438.domain.UserRepository;
 import com.cst438.dto.AssignmentDTO;
 import com.cst438.dto.AssignmentStudentDTO;
 import com.cst438.dto.SectionDTO;
+import com.cst438.service.RegistrarServiceProxy;
 
 import jakarta.validation.Valid;
 
@@ -30,17 +38,20 @@ public class AssignmentController {
     private final AssignmentRepository assignmentRepository;
     private final GradeRepository gradeRepository;
     private final UserRepository userRepository;
+    private final RegistrarServiceProxy registrar;
 
     public AssignmentController(
             SectionRepository sectionRepository,
             AssignmentRepository assignmentRepository,
             GradeRepository gradeRepository,
-            UserRepository userRepository
+            UserRepository userRepository,
+            RegistrarServiceProxy registrar
     ) {
         this.sectionRepository = sectionRepository;
         this.assignmentRepository = assignmentRepository;
         this.gradeRepository = gradeRepository;
         this.userRepository = userRepository;
+        this.registrar = registrar;
     }
 
     // get Sections for an instructor
@@ -94,14 +105,56 @@ public class AssignmentController {
     @PreAuthorize("hasAuthority('SCOPE_ROLE_INSTRUCTOR')")
     public AssignmentDTO createAssignment(
             @Valid @RequestBody AssignmentDTO dto,
-            Principal principal) {
+            Principal principal) throws Exception {
         
         //  user must be the instructor for the Section
 		//  check that assignment dueDate is between start date and 
 		//  end date of the term
 		//  create and save an Assignment entity
         //  return AssignmentDTO with database generated primary key
-        return null;
+
+        
+        // Instructor information
+        String instructorEmail = principal.getName();
+        
+        // Get section from the database
+        Section section = sectionRepository.findById(dto.secNo()).orElse(null);
+
+        // Check validity of Assignment parameters
+        // Check that section exists
+        if (section == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid section");
+        }
+        // Check that the user is the instructor for the section
+        if (!instructorEmail.equals(section.getInstructorEmail())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid instructor email");
+        }
+        // Check that due date is valid
+        // Get term
+        Term term = section.getTerm();
+        Date dueDate = Date.valueOf(dto.dueDate());
+        int startCompare = term.getStartDate().compareTo(dueDate);
+        int endCompare = term.getEndDate().compareTo(dueDate);
+        if (startCompare > 0 || endCompare < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid due date");
+        }
+
+        // Create and save Assignment
+        Assignment a = new Assignment();
+        a.setTitle(dto.title());
+        a.setDueDate(dueDate);
+        a.setSection(section);
+        assignmentRepository.save(a);
+
+        // return AssignmentDTO
+        return new AssignmentDTO(
+            a.getAssignmentId(),
+            a.getTitle(),
+            a.getDueDate().toString(),
+            section.getCourse().getCourseId(),
+            section.getSectionId(),
+            section.getSectionNo()
+        );
     }
 
 
@@ -110,6 +163,7 @@ public class AssignmentController {
     public AssignmentDTO updateAssignment(@Valid @RequestBody AssignmentDTO dto, Principal principal) {
         //  update Assignment Entity.  only title and dueDate fields can be changed.
         //  user must be instructor of the Section
+        
         return null;
     }
 
